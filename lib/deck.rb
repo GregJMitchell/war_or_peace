4ca30e1830b4c88d705f9177a19f2fd8fda2b0ae
Greg Mitchell <g2mitchell3003@gmail.com>
2020-08-20 08:55:08 -0600

    Edit deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,25 +3,28 @@ class Deck
   def initialize(cards)
     @cards = cards
   end
+
   def rank_of_card_at(index)
     @cards[index].rank
   end
+
   def high_ranking_cards
     high_cards = []
     @cards.each do |card|
-      if card.rank >= 11
-        high_cards << card
-      end
+      card.rank < 11 || high_cards << card
     end
-     high_cards
+    high_cards
   end
+
   def percent_high_ranking
-    percent_high = (high_ranking_cards.length.to_f / @cards.length.to_f) * 100
+    percent_high = (high_ranking_cards.length.to_f / @cards.length) * 100
     percent_high
   end
+
   def remove_card
     @cards.shift
   end
+
   def add_card(card)
     @cards << card
   end
