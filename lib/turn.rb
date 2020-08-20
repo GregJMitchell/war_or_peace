3cbc46b6553968214cc1bf514c23504b67b834e2
Greg Mitchell <g2mitchell3003@gmail.com>
2020-08-20 15:13:21 -0600

    Add war and mad to winner method and tests for war and mad

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,24 +9,43 @@ class Turn
      def type
         if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
             :basic
-        elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+        elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) &&
+              @player1.deck.rank_of_card_at(2) != @player2.deck.rank_of_card_at(2)
             :war
-        elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+        else
             :mutually_assured_destruction
         end
      end
 
      def find_winner_basic
         if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
-            @player1
+            @player1.deck
         elsif @player1.deck.rank_of_card_at(0) < @player2.deck.rank_of_card_at(0)
-            @player2
+            @player2.deck
         end
      end
 
+     def find_winner_war
+        if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+            @player1.deck
+        elsif @player1.deck.rank_of_card_at(2) < @player2.deck.rank_of_card_at(2)
+            @player2.deck
+        end
+     end
+
+     def find_winner_mad
+        'No Winner'
+     end
+
      def winner
-        if type == :basic
+        mad_result = find_winner_mad
+        case type
+        when :basic
             find_winner_basic
+        when :war
+            find_winner_war
+        else
+          mad_result
         end
      end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -76,6 +76,29 @@ class TurnTest <Minitest::Test
         turn = Turn.new(player1, player2)
 
         assert_equal :basic, turn.type
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card4, card3, card6, card7])
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+        turn2 = Turn.new(player1, player2)
+        assert_equal :war, turn2.type
+
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, '8', 8)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card4, card3, card6, card7])
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+        turn3 = Turn.new(player1, player2)
+
+        assert_equal :mutually_assured_destruction, turn3.type
     end
 
     def test_it_can_find_winner
@@ -92,8 +115,31 @@ class TurnTest <Minitest::Test
         player1 = Player.new("Megan", deck1)
         player2 = Player.new("Aurora", deck2)
         turn = Turn.new(player1, player2)
-        turn.type
 
-        assert_equal "Megan", turn.winner.name
+        assert_equal player1.deck, turn.winner
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card4, card3, card6, card7])
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+        turn2 = Turn.new(player1, player2)
+
+        assert_equal player2.deck, turn2.winner
+
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, '8', 8)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card4, card3, card6, card7])
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+        turn3 = Turn.new(player1, player2)
+
+        assert_equal 'No Winner', turn3.winner
     end
 end
