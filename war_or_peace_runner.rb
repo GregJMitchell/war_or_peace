7d783fbb4bfcb44ee09caa2f6ebaa5edd486fd4e
Greg Mitchell <g2mitchell3003@gmail.com>
2020-08-22 15:32:18 -0600

    Move war or peace runner and remove comments

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,5 +1,3 @@
-#require 'minitest/autorun'
-#require 'minitest/pride'
 require './lib/player'
 require './lib/card'
 require './lib/deck'
@@ -7,7 +5,4 @@ require './lib/turn'
 require './lib/game'
 
 game = Game.new
-
-# game.create_decks
-# puts game.big_deck.length
 game.start
