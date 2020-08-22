4cd3a6f94ec94eaeedff465fe1f7aa21bd5d5d97
Greg Mitchell <g2mitchell3003@gmail.com>
2020-08-21 20:20:57 -0600

    Create tests for game class. Update game class.

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,11 @@
 class Game
-    attr_reader :big_deck, :deck1, :deck2
+    attr_reader :big_deck, :deck1, :deck2, :player1, :player2
     def initialize
         @big_deck = []
         @deck1 = deck1
         @deck2 = deck2
+        @player1 = player1
+        @player2 = player2
     end
 
     def create_decks
@@ -12,7 +14,7 @@ class Game
         suits.each do |suit|
             2.upto(14) do |number|
                 if number < 11
-                    @big_deck << Card.new(suit, "#{number}", number)
+                    @big_deck << Card.new(suit, '#{number}', number)
                 elsif number == 11
                     @big_deck << Card.new(suit, 'Jack', number)
                 elsif number == 12
@@ -27,11 +29,9 @@ class Game
         @big_deck.shuffle
         @deck1 = Deck.new@big_deck[0..25]
         @deck2 = Deck.new@big_deck[26..52]
-    end
 
-    def create_players
-        player1 = Player.new("Megan", @deck1)
-        player2 = Player.new("Aurora", @deck2)
+        @player1 = Player.new("Megan", @deck1)
+        @player2 = Player.new("Aurora", @deck2)
     end
 
     def start_screen
@@ -51,7 +51,6 @@ class Game
         start_screen
         input = gets.chomp
         if input.capitalize == 'Go'
-            create_players
             create_decks
             puts "Worx"
         else
--- test/game_test.rb
+++ test/game_test.rb
@@ -8,12 +8,35 @@ require './lib/game'
 
 class ClassTest <Minitest::Test
     def setup
-        
+        game = Game.new
     end
 
-    def test_it_has_readable_attributes
+    def test_it_exists
         game = Game.new
 
-        assert_instance of Game, game
+        assert_instance_of Game, game
+    end
+    
+    def test_has_readable_attributes
+        game = Game.new
+
+        assert_equal [], game.big_deck
+        assert_nil game.deck1
+        assert_nil game.deck2
+    end
+
+    def test_it_can_create_deck
+        game = Game.new
+        game.create_decks
+
+        refute_equal [], game.big_deck 
+    end
+
+    def test_it_can_create_players
+        game = Game.new
+        game.create_decks
+
+        assert_equal "Megan", game.player1.name
+        assert_equal "Aurora", game.player2.name
     end
 end
