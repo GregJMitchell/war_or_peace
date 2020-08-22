7df080ec6e7c7b6cfaacb48d8d4c8c33b5e7759b
Greg Mitchell <g2mitchell3003@gmail.com>
2020-08-22 14:26:42 -0600

    Update deck class tests.

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,40 +4,28 @@ require './lib/card'
 require './lib/deck'
 
 class DeckTest <Minitest::Test
-  def setup
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-  end
   def test_it_exists
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
+    deck = Deck.new([])
 
     assert_instance_of Deck, deck
   end
+
   def test_it_has_readable_attributes
     card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
+    cards = [card1]
     deck = Deck.new(cards)
 
-    assert_equal [card1, card2, card3], deck.cards
+    assert_equal [card1], deck.cards
   end
+  
   def test_it_can_find_rank
     card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
+    cards = [card1]
     deck = Deck.new(cards)
 
-    assert_equal 3, deck.rank_of_card_at(1)
+    assert_equal 12, deck.rank_of_card_at(0)
   end
+
   def test_it_can_find_high_ranking_cards
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
@@ -48,17 +36,17 @@ class DeckTest <Minitest::Test
 
     assert_equal [card1, card3], deck.high_ranking_cards
   end
+
   def test_it_can_find_percent_high_cards
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
-    deck.cards
-    deck.high_ranking_cards
-
+    
     assert_equal 66.67, deck.percent_high_ranking
   end
+
   def test_can_it_remove_card
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
@@ -69,6 +57,7 @@ class DeckTest <Minitest::Test
 
     assert_equal [card2, card3], deck.cards
   end
+
   def test_it_can_add_card
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
@@ -76,7 +65,8 @@ class DeckTest <Minitest::Test
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
     card4 = Card.new(:club, '5', 5)
+    deck.add_card(card4)
 
-    assert_equal [card1, card2, card3, card4], deck.add_card(card4)
+    assert_equal [card1, card2, card3, card4], deck.cards
   end
 end
